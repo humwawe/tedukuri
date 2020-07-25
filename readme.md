d027db9df8fdf503d89644a01bee773b8831056d
humwawe <wawe8963098@163.com>
2020-07-25 17:58:13 +0800

    a^b

---
+++ readme.md
@@ -0,0 +1,2 @@
+# 0x00
+[0x00](https://github.com/humwawe/tedukuri/blob/master/0x00.md)
---
+++ src/main/java/ox01/AXorB.java
@@ -0,0 +1,21 @@
+package ox01;
+
+import common.io.InputReader;
+import common.io.OutputWriter;
+
+public class AXorB {
+    public void solve(int testNumber, InputReader in, OutputWriter out) {
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int p = in.nextInt();
+        long res = 1 % p;
+        while (b != 0) {
+            if ((b & 1) == 1) {
+                res = res * a % p;
+            }
+            a = (int) ((long) a * a % p);
+            b = b >> 1;
+        }
+        out.println(res);
+    }
+}
