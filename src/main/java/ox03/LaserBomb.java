119a7f090ceff4bd807b18ddcc27f1116c829ffd
humwawe <wawe8963098@163.com>
2020-08-01 23:56:56 +0800

    激光炸弹

---
+++ src/main/java/ox03/LaserBomb.java
@@ -0,0 +1,9 @@
+package ox03;
+
+import common.io.InputReader;
+import common.io.OutputWriter;
+
+public class LaserBomb {
+    public void solve(int testNumber, InputReader in, OutputWriter out) {
+    }
+}
