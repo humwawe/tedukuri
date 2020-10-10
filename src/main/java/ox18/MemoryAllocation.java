6b4300fb78ce511131cde308c52e975d3dbd3694
humwawe <wawe8963098@163.com>
2020-10-10 23:51:13 +0800

    内存分配

---
+++ src/main/java/ox18/MemoryAllocation.java
@@ -0,0 +1,9 @@
+package ox18;
+
+import common.io.InputReader;
+import common.io.OutputWriter;
+
+public class MemoryAllocation {
+    public void solve(int testNumber, InputReader in, OutputWriter out) {
+    }
+}
