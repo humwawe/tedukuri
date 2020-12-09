69381e8d005c792ca09d3a9990a4ff6b99b71a23
humwawe <wawe8963098@163.com>
2020-12-10 00:02:55 +0800

    可达性统计

--- src/main/java/ox21/AccessibilityStatistics.java
+++ src/main/java/ox21/AccessibilityStatistics.java
@@ -3,10 +3,8 @@ package ox21;
 import common.io.InputReader;
 import common.io.OutputWriter;
 
-import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.BitSet;
-import java.util.Queue;
 
 public class AccessibilityStatistics {
     int N = 30005;
@@ -15,45 +13,81 @@ public class AccessibilityStatistics {
     int[] ne = new int[N];
     int[] deg = new int[N];
     int idx;
+    BitSet[] set = new BitSet[N];
+    boolean[] vis = new boolean[N];
 
     public void solve(int testNumber, InputReader in, OutputWriter out) {
         Arrays.fill(h, -1);
         idx = 0;
         int n = in.nextInt();
         int m = in.nextInt();
+//        for (int i = 0; i < m; i++) {
+//            int a = in.nextInt();
+//            int b = in.nextInt();
+//            add(b, a);
+//        }
+//
+//        BitSet[] bitSet = new BitSet[n + 1];
+//        for (int i = 0; i <= n; i++) {
+//            bitSet[i] = new BitSet();
+//        }
+//        Queue<Integer> queue = new ArrayDeque<>();
+//        for (int i = 1; i <= n; i++) {
+//            if (deg[i] == 0) {
+//                queue.add(i);
+//            }
+//            bitSet[i].set(i);
+//        }
+//        while (!queue.isEmpty()) {
+//            Integer cur = queue.poll();
+//            for (int i = h[cur]; i != -1; i = ne[i]) {
+//                int j = e[i];
+//                bitSet[j].or(bitSet[cur]);
+//                deg[j]--;
+//                if (deg[j] == 0) {
+//                    queue.add(j);
+//                }
+//            }
+//        }
+//        for (int i = 1; i <= n; i++) {
+//            out.println(bitSet[i].cardinality());
+//        }
+
+
         for (int i = 0; i < m; i++) {
             int a = in.nextInt();
             int b = in.nextInt();
-            add(b, a);
+            add(a, b);
         }
-
-        BitSet[] bitSet = new BitSet[n + 1];
         for (int i = 0; i <= n; i++) {
-            bitSet[i] = new BitSet();
+            set[i] = new BitSet();
         }
-        Queue<Integer> queue = new ArrayDeque<>();
         for (int i = 1; i <= n; i++) {
-            if (deg[i] == 0) {
-                queue.add(i);
-            }
-            bitSet[i].set(i);
-        }
-        while (!queue.isEmpty()) {
-            Integer cur = queue.poll();
-            for (int i = h[cur]; i != -1; i = ne[i]) {
-                int j = e[i];
-                bitSet[j].or(bitSet[cur]);
-                deg[j]--;
-                if (deg[j] == 0) {
-                    queue.add(j);
-                }
+            if (!vis[i]) {
+                dfs(i);
             }
         }
         for (int i = 1; i <= n; i++) {
-            out.println(bitSet[i].cardinality());
+            out.println(set[i].cardinality());
         }
 
     }
+
+    private void dfs(int cur) {
+        set[cur].set(cur);
+        vis[cur] = true;
+        for (int i = h[cur]; i != -1; i = ne[i]) {
+            int j = e[i];
+            if (vis[j]) {
+                set[cur].or(set[j]);
+                continue;
+            }
+            dfs(j);
+            set[cur].or(set[j]);
+        }
+
+    }
+
 
     private void add(int a, int b) {
         e[idx] = b;
