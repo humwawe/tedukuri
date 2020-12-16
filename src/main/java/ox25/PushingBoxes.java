b80784ce38d94d2be186009b2a310974f56ad344
humwawe <wawe8963098@163.com>
2020-12-17 00:50:31 +0800

    Pushing Boxes

--- src/main/java/ox25/PushingBoxes.java
+++ src/main/java/ox25/PushingBoxes.java
@@ -9,10 +9,11 @@ import java.util.Queue;
 public class PushingBoxes {
     char[][] map;
     int n, m;
-    char dirBox[] = {'N', 'S', 'W', 'E'};
-    char dirMan[] = {'n', 's', 'w', 'e'};
-    int dx[] = {-1, 1, 0, 0};
-    int dy[] = {0, 0, -1, 1};
+    char[] dirBox = {'N', 'S', 'W', 'E'};
+    char[] dirMan = {'n', 's', 'w', 'e'};
+    int[] dx = {-1, 1, 0, 0};
+    int[] dy = {0, 0, -1, 1};
+    String res;
 
     public void solve(int testNumber, InputReader in, OutputWriter out) {
         n = in.nextInt();
@@ -21,6 +22,7 @@ public class PushingBoxes {
             return;
         }
         map = new char[n][m];
+        res = "Impossible.";
         Point box = new Point();
         Point tar = new Point();
         for (int i = 0; i < n; i++) {
@@ -43,7 +45,7 @@ public class PushingBoxes {
             }
         }
         out.println("Maze #" + testNumber);
-        out.println(boxBfs(box, tar));
+        out.println(boxBfs(new Point(box), new Point(tar)));
         out.println();
 
     }
@@ -68,7 +70,7 @@ public class PushingBoxes {
                 Point next = new Point();
                 next.mr = cur.mr + dx[i];
                 next.mc = cur.mc + dy[i];
-                next.way = cur.way;
+                next.way = new StringBuilder(cur.way);
                 if (!judgeEdge(next.mr, next.mc) || vis[next.mr][next.mc] || (next.mr == beg.r && next.mc == beg.c)) {
                     continue;
                 }
@@ -87,7 +89,9 @@ public class PushingBoxes {
         while (!que.isEmpty()) {
             Point cur = que.poll();
             if (cur.r == tar.r && cur.c == tar.c) {
-                return cur.way.toString();
+                if ("Impossible.".equals(res) || comp(cur.way.toString(), res)) {
+                    res = cur.way.toString();
+                }
             }
             for (int i = 0; i < 4; i++) {
                 Point next = new Point();
@@ -97,7 +101,7 @@ public class PushingBoxes {
                 // 人的下一个坐标
                 next.mr = cur.r - dx[i];
                 next.mc = cur.c - dy[i];
-                next.way = cur.way;
+                next.way = new StringBuilder(cur.way);
                 if (!judgeEdge(next.r, next.c)) {
                     continue;
                 }
@@ -108,8 +112,8 @@ public class PushingBoxes {
                 if (vis[i][next.r][next.c]) {
                     continue;
                 }
-                String mway = manBfs(cur, next);
-                if (!mway.equals("Impossible.")) {
+                String mway = manBfs(new Point(cur), new Point(next));
+                if (!"Impossible.".equals(mway)) {
                     next.way.append(mway);
                     next.way.append(dirBox[i]);
                     // 箱子已经移动，人的位置为原来箱子的位置
@@ -120,13 +124,54 @@ public class PushingBoxes {
                 }
             }
         }
-        return "Impossible.";
+        return res;
+    }
+
+    private boolean comp(String cur, String res) {
+        int c1 = 0;
+        int c2 = 0;
+        int r1 = 0;
+        int r2 = 0;
+        for (char c : cur.toCharArray()) {
+            if (c >= 'A' && c <= 'Z') {
+                c1++;
+            } else {
+                c2++;
+            }
+        }
+        for (char c : res.toCharArray()) {
+            if (c >= 'A' && c <= 'Z') {
+                r1++;
+            } else {
+                r2++;
+            }
+        }
+        if (c1 < r1) {
+            return true;
+        }
+        if (c1 == r1) {
+            return c2 < r2;
+        }
+        return false;
+
     }
 
     class Point {
         StringBuilder way;
         int r, c; // 箱子的位置
         int mr, mc; // 人的位置
+
+        public Point() {
+            way = new StringBuilder();
+        }
+
+        public Point(Point next) {
+            r = next.r;
+            c = next.c;
+            mr = next.mr;
+            mc = next.mc;
+            way = new StringBuilder(next.way);
+        }
     }
 
 }
