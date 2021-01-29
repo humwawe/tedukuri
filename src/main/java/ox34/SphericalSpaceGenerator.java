460b8d3cb56db74592c3d7acae147941a00742b1
humwawe <wawe8963098@163.com>
2021-01-29 22:49:30 +0800

    球形空间产生器 开关问题

---
+++ src/main/java/ox34/SphericalSpaceGenerator.java
@@ -0,0 +1,87 @@
+package ox34;
+
+import common.io.InputReader;
+import common.io.OutputWriter;
+
+public class SphericalSpaceGenerator {
+    int N = 15;
+    int n;
+    double[][] a = new double[N][N + 1];
+    double eps = 1e-6;
+
+    public void solve(int testNumber, InputReader in, OutputWriter out) {
+        n = in.nextInt();
+        double[][] b = new double[n + 1][n];
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < n; j++) {
+                b[i][j] = in.nextDouble();
+            }
+        }
+        double[] c = new double[n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = 2 * (b[i][j] - b[i + 1][j]);
+                c[i] += b[i][j] * b[i][j] - b[i + 1][j] * b[i + 1][j];
+            }
+            a[i][n] = c[i];
+        }
+        gauss();
+        for (int i = 0; i < n; i++) {
+            out.printf("%.3f ", a[i][n]);
+        }
+    }
+
+    int gauss() {
+        int c, r;
+        for (c = 0, r = 0; c < n; c++) {
+            int t = r;
+            // 找到绝对值最大的行
+            for (int i = r; i < n; i++) {
+                if (Math.abs(a[i][c]) > Math.abs(a[t][c])) {
+                    t = i;
+                }
+            }
+            if (Math.abs(a[t][c]) < eps) {
+                continue;
+            }
+            // 将绝对值最大的行换到最顶端
+            for (int i = c; i <= n; i++) {
+                double tmp = a[t][i];
+                a[t][i] = a[r][i];
+                a[r][i] = tmp;
+            }
+            // 将当前上的首位变成1
+            for (int i = n; i >= c; i--) {
+                a[r][i] /= a[r][c];
+            }
+            // 用当前行将下面所有的列消成0
+            for (int i = r + 1; i < n; i++) {
+                if (Math.abs(a[i][c]) > eps) {
+                    for (int j = n; j >= c; j--) {
+                        a[i][j] -= a[r][j] * a[i][c];
+                    }
+                }
+            }
+            r++;
+        }
+
+        if (r < n) {
+            for (int i = r; i < n; i++) {
+                if (Math.abs(a[i][n]) > eps) {
+                    // 无解
+                    return 2;
+                }
+            }
+            // 有无穷多组解
+            return 1;
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                a[i][n] -= a[i][j] * a[j][n];
+            }
+        }
+        // 有唯一解
+        return 0;
+    }
+}
---
+++ src/main/java/ox34/SwitchProblem.java
@@ -0,0 +1,66 @@
+package ox34;
+
+import common.io.InputReader;
+import common.io.OutputWriter;
+
+public class SwitchProblem {
+    public void solve(int testNumber, InputReader in, OutputWriter out) {
+        int n = in.nextInt();
+        int[] a = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            a[i] = in.nextInt();
+        }
+        for (int i = 1; i <= n; i++) {
+            int tmp = in.nextInt();
+            // a[i][0] 存放方程右边的常量
+            a[i] ^= tmp;
+            // a[i][i]=1
+            a[i] |= 1 << i;
+        }
+        int x = in.nextInt();
+        int y = in.nextInt();
+        while (x != 0 && y != 0) {
+            // a[y][x]=1
+            a[y] |= 1 << x;
+            x = in.nextInt();
+            y = in.nextInt();
+        }
+        int res = 1;
+        for (int i = 1; i <= n; i++) {
+            // 找最大的主元
+            for (int j = i + 1; j <= n; j++) {
+                if (a[j] > a[i]) {
+                    int tmp = a[i];
+                    a[i] = a[j];
+                    a[j] = tmp;
+                }
+            }
+            // 消元完毕，有i-1个主元
+            if (a[i] == 0) {
+                res = 1 << (n - i + 1);
+                break;
+            }
+            // 出现0=1的方程
+            if (a[i] == 1) {
+                res = 0;
+                break;
+            }
+            for (int k = n; k > 0; k--) {
+                if ((a[i] >> k & 1) == 1) {
+                    for (int j = 1; j <= n; j++) {
+                        if (i != j && (a[j] >> k & 1) == 1) {
+                            a[j] ^= a[i];
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+        if (res == 0) {
+            out.println("Oh,it's impossible~!!");
+        } else {
+            out.println(res);
+        }
+
+    }
+}
