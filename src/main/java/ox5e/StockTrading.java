fe16f466d47d303b5a510364b25e9a5fe9987fcd
humwawe <wawe8963098@163.com>
2021-06-30 00:09:09 +0800

    股票交易

--- src/main/java/ox5e/StockTrading.java
+++ src/main/java/ox5e/StockTrading.java
@@ -26,7 +26,7 @@ public class StockTrading {
         int inf = 0x3f3f3f3f;
         int[][] f = new int[n + 1][maxP + 1];
         Arrays.fill(f[0], -inf);
-
+        f[0][0] = 0;
         Deque<Integer> deque = new LinkedList<>();
 
         for (int i = 1; i <= n; i++) {
@@ -43,6 +43,7 @@ public class StockTrading {
                 continue;
             }
             // 买入
+            deque.clear();
             for (int j = 0; j <= maxP; j++) {
                 while (!deque.isEmpty() && deque.getFirst() < j - as[i]) {
                     deque.pollFirst();
@@ -59,19 +60,20 @@ public class StockTrading {
             // 卖出
             deque.clear();
             for (int j = maxP; j >= 0; j--) {
-                while (!deque.isEmpty() && deque.getFirst() < j - as[i]) {
+                while (!deque.isEmpty() && deque.getFirst() > j + bs[i]) {
                     deque.pollFirst();
                 }
                 if (!deque.isEmpty()) {
-                    f[i][j] = Math.max(f[i][j], f[lDay][deque.getFirst()] + ap[i] * deque.getFirst() - ap[i] * j);
+                    f[i][j] = Math.max(f[i][j], f[lDay][deque.getFirst()] + bp[i] * deque.getFirst() - bp[i] * j);
                 }
 
-                while (!deque.isEmpty() && f[lDay][deque.getLast()] + ap[i] * deque.getLast() <= f[lDay][j] + ap[i] * j) {
+                while (!deque.isEmpty() && f[lDay][deque.getLast()] + bp[i] * deque.getLast() <= f[lDay][j] + bp[i] * j) {
                     deque.pollLast();
                 }
                 deque.addLast(j);
             }
         }
+        out.println(f[n][0]);
 
     }
 }
