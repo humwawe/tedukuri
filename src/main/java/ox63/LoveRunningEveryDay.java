ab2a2ccb5c1a22d4222f4d276736680eb42094e1
humwawe <wawe8963098@163.com>
2021-08-20 23:57:54 +0800

    天爱跑步

---
+++ src/main/java/ox63/LoveRunningEveryDay.java
@@ -0,0 +1,9 @@
+package ox63;
+
+import common.io.InputReader;
+import common.io.OutputWriter;
+
+public class LoveRunningEveryDay {
+    public void solve(int testNumber, InputReader in, OutputWriter out) {
+    }
+}
